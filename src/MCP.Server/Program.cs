ebb4bb8cb42f2216dca592b897f20942466a7bf0
Callum Whyte <hey@callumwhyte.com>
2025-09-10 07:42:23 +0000

    Add Semantic Kernel

--- src/MCP.Server/Program.cs
+++ src/MCP.Server/Program.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using MCP_Server;
+
+// Create a kernel builder and add plugins
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+kernelBuilder.Plugins.AddFromType<MCP_Server.Plugins.VenuePlugin>();
+
+// Build the kernel
+Kernel kernel = kernelBuilder.Build();
+
 var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
 builder.Services.AddMcpServer()
     .WithStdioServerTransport()
     .WithToolsFromAssembly()
-    .WithResourcesFromAssembly();
+    .WithResourcesFromAssembly()
+    .WithTools(kernel);
 
 var app = builder.Build();
 
