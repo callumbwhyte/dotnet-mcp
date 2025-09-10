7e50557bb83c86efba85065c35f1776e5acfb984
Callum Whyte <hey@callumwhyte.com>
2025-09-10 07:42:23 +0000

    Add Semantic Kernel

---
+++ src/MCP.Server/Extensions/KernelExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.SemanticKernel;
+using ModelContextProtocol.Server;
+
+namespace MCP_Server;
+
+public static class KernelExtensions
+{
+    public static IMcpServerBuilder WithTools(this IMcpServerBuilder builder, Kernel kernel)
+    {
+        foreach (var plugin in kernel.Plugins)
+        {
+            foreach (var function in plugin)
+            {
+                builder.Services.AddSingleton(McpServerTool.Create(function.AsAIFunction()));
+            }
+        }
+
+        return builder;
+    }
+}
